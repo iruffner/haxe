7f81b2f820e14b64d97f0d2fc5bacff78744b492
leventekalman <lw.lewy@gmail.com>
2015-03-13 08:18:53 -0400

    BI-163: Tests for m3.util.SizedMap

--- m3/test/SizedMapTest.hx
+++ m3/test/SizedMapTest.hx
@@ -1,15 +1,42 @@
 package m3.test;
 
+import m3.util.SizedMap;
+import m3.test.Assert;
+
+/*
+new is missing, becuase we already create a new map in every other test case
+*/
+
 @:rtti class SizedMapTest{
-  @test
-  function newTest() {
-  }
 
   @test
   function setTest() {
+  	var map = new SizedMap();
+
+	map.set('a', 'b');
+
+	if(!map.exists('a'))
+	{
+		Assert.fail("Item was not set");
+	}
   }
 
   @test
   function removeTest() {
+  	var map = new SizedMap();
+
+  	map.set('a', 'b');
+
+  	if(!map.exists('a'))
+	{
+		Assert.fail("Item was not set");
+	}
+
+  	map.remove('a');
+
+	if(map.exists('a'))
+	{
+		Assert.fail("Item was not deleted");
+	}
   }
 }
