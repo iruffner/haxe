9aae4173344b3a42faf894a2f4abededce2e48b8
Dave Kruse <dave@krusetech.com>
2015-07-02 14:06:04 -0500

    Add iterable collector.

--- m3/util/LambdaX.hx
+++ m3/util/LambdaX.hx
@@ -14,14 +14,24 @@ class LambdaX {
 		}
 	}
 
-	// /**
-	// 	Iterate with an index
-	// **/
-	// public static function iteria<A>( ar : Array<A>, f : A -> Int -> Void ) {
-	// 	for( i in 0...ar.length ) {			
-	// 		f(ar[i], i);
-	// 	}
-	// }
+	/**
+		Collect from iterable where lambda = true
+		example:
+		things.collectX(function(thing){
+			return thing.value == true;
+		});
+	**/
+	public static function collectX<A>(it: Iterable<A>, f: Dynamic->Bool) {
+		var collection: Array<Dynamic> = [];
+		var i = 0;
+		for (x in it) {
+			var result: Bool = f(x);
+			if (result == true) {
+				collection.push(x);
+			}
+			i += 1;
+		}
+		return collection;
+	}
 
 }
-
